dacbba9d2360f0ea30c5279c59699ba9a3de11bb
Subhana <subhana5721194@gmail.com>
2023-06-09 08:48:28 +0600

    usecart Added

---
+++ src/Hook/UseCarts.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { useQuery } from '@tanstack/react-query'
+import { AuthContext } from '../Providers/AuthProvider';
+
+const UseCarts = () => {
+    const {user} = useContext(AuthContext);
+
+    const { isLoading, refetch, data: cart = [] } = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async () =>{
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            return res.json();
+        },
+      })
+
+      return [cart, refetch, isLoading]
+};
+
+export default UseCarts;
